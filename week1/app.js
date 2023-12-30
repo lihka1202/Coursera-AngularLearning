c4392c95915f03d2152f8dc0b348ca2ef3b5ec38
Akhil Muthya Narayanachary <akhil2021@Akhils-Mac-mini.local>
2023-12-30 17:53:49 +0800

    chore: Interpolation Part 1 beg code

--- week1/app.js
+++ week1/app.js
@@ -1,20 +1,12 @@
 (function () {
-    'use strict';
-    
-    angular.module('DIApp', [])
-    .controller('DIController', DIController);
-
-    DIController.$inject = ['$scope', '$filter']
-    
-    function DIController ($scope, $filter) {
-      $scope.name = "Yaakov";
-
-      // Converts everything to uppercase when cursor is not focused
-      // on the input field
-      $scope.upper = function () {
-        var upCase = $filter('uppercase');
-        $scope.name = upCase($scope.name);
-      }
-    }
-    
-    })();
+  'use strict';
+  
+  angular.module('MsgApp', [])
+  .controller('MsgController', MsgController);
+  
+  MsgController.$inject = ['$scope'];
+  function MsgController($scope) {
+    $scope.name = "Yaakov";
+  }
+  
+  })();
